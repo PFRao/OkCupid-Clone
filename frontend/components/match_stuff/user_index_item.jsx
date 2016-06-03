a560dd741123129c0468fe7e8124797b30479f70
pfrao <prao3@masonlive.gmu.edu>
2016-06-02 22:08:08 -0400

    fixed duplicate matches and age display

--- frontend/components/match_stuff/user_index_item.jsx
+++ frontend/components/match_stuff/user_index_item.jsx
@@ -5,11 +5,12 @@ var UserIndexItem = React.createClass({
   render: function () {
     // debugger
     var oldness = (new Date()) - (new Date(this.props.person.birthdate));
+    var oldness2 = Math.floor(oldness / 31536000000);
     return (
       <li>
         <img src={window.peterImage} />
         <h3>{this.props.person.username}</h3>
-        <p>Age {oldness}</p>
+        <p>Age {oldness2}</p>
         <p>{this.props.rating} % Match</p>
       </li>
     );
